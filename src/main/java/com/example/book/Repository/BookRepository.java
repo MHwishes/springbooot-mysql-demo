b0d8ad3a18c16c10cdaea887db5951c4de15af6f
Hong Ma <hong.ma@rea-group.com>
2019-12-23 09:50:06 +0800

    add put and delete get request controller

--- src/main/java/com/example/book/Repository/BookRepository.java
+++ src/main/java/com/example/book/Repository/BookRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public  interface BookRepository extends JpaRepository<Book,Long> {
-  Book findById(long id);
-  List<Book> findByName(String name);
+  Book findByAuthor(String author);
+
+//  List<Book> findByName(String name);
 
 }
--- src/main/java/com/example/book/controllers/BookController.java
+++ src/main/java/com/example/book/controllers/BookController.java
@@ -3,10 +3,12 @@ package com.example.book.controllers;
 import com.example.book.Repository.BookRepository;
 import com.example.book.domain.Book;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/books")
@@ -21,8 +23,40 @@ public class BookController {
     return bookRepository.save(book);
   }
 
+  @RequestMapping(value="/",method = RequestMethod.GET)
+  public List<Book> getAllBooks() {
+    return bookRepository.findAll();
+  }
+
   @RequestMapping("/{bookId}")
-  public Book findBookByID(long bookId) {
+  public Optional<Book> findBookByID(@PathVariable Long bookId) {
     return bookRepository.findById(bookId);
+  }
+
+  @RequestMapping(value="/{author}",method = RequestMethod.GET)
+  public Book findBookByAuthor(@PathVariable String author) {
+
+    return bookRepository.findByAuthor(author);
+  }
+
+  @RequestMapping(value="/{bookId}",method = RequestMethod.DELETE)
+  public ResponseEntity deleteBook(@PathVariable Long bookId) {
+    Optional<Book> book=bookRepository.findById(bookId);
+    if(book.isPresent()){
+       bookRepository.deleteById(bookId);
+    }
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @RequestMapping(value="/{bookId}",method = RequestMethod.PUT)
+  public ResponseEntity updateBook(@PathVariable Long bookId,@RequestBody Book book) {
+   Book currentBook= bookRepository.findById(bookId).orElse(null);;
+   
+    if(currentBook==null){
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    currentBook.setName(book.getName());
+    currentBook.setAuthor(book.getAuthor());
+    return new ResponseEntity<>(currentBook,HttpStatus.NO_CONTENT);
   }
 }
